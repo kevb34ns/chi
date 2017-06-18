4a539967be62780b490be3315a5f23d7033203f3
Kevin Yang <yangkevi@usc.edu>
2017-06-17 21:18:59 -0700

    refactored frontend search query to a service

--- zidian/public/javascripts/angularApp.js
+++ zidian/public/javascripts/angularApp.js
@@ -14,36 +14,50 @@ app.factory('DataService', function(){
 	};
 });
 
-app.controller('MainController', ['$scope', '$window', 'DataService', function($scope, $window, DataService) {
-
-	$scope.isSimplified = DataService.isSimplified;
-
-	$scope.search = function(searchTerm) {
-		if (!$scope.searchTerm || $scope.searchTerm === '') {
-			return;
+app.factory('SearchService', ['$http', function($http) {
+	return {
+		/**
+		 * Returns an array of results for the specified query.
+		 * @param searchTerm The term to query the database for.
+		 * @return an array of objects, each of which represents a term.
+		 */
+		search: function(searchTerm) {
+			return $http.get("http://localhost:3000/api/search/" + searchTerm).then(
+				function(response) {
+					return response.data;
+				}
+			)
 		}
-		console.log($scope.searchTerm);
-		$window.location.href = '#/search/' + $scope.searchTerm;
-		$scope.searchTerm = '';
 	}
-
 }]);
 
-app.controller('SearchController', ['$scope', '$rootScope', '$http', '$stateParams', 'DataService', function($scope, $rootScope, $http, $stateParams, DataService) {
+app.controller('MainController', ['$scope', '$window', 'DataService', 
+	function($scope, $window, DataService) {
+
+		$scope.isSimplified = DataService.isSimplified;
+
+		$scope.search = function(searchTerm) {
+			if (!$scope.searchTerm || $scope.searchTerm === '') {
+				return;
+			}
+			console.log($scope.searchTerm);
+			$window.location.href = '#/search/' + $scope.searchTerm;
+			$scope.searchTerm = '';
+		}
+	}
+]);
+
+app.controller('SearchController', ['$scope', '$rootScope', '$http', '$stateParams', 'DataService', 'SearchService', function($scope, $rootScope, $http, $stateParams, DataService, SearchService) {
 
 	$scope.isSimplified = DataService.isSimplified;
 
-	$http.get("http://localhost:3000/api/search/" + $stateParams.term)
-		.success(function(result) {
-			$scope.entries = result;
-			angular.forEach($scope.entries, function(entry) {
-				entry.pinyin = pinyinify(entry.pinyin);	
-			});
+	SearchService.search($stateParams.term).then(function(result) {
+		console.log(result);
+		$scope.entries = result;
+		angular.forEach($scope.entries, function(entry) {
+			entry.pinyin = pinyinify(entry.pinyin);	//TODO may violate view/controller separation; package pinyinify using node, perhaps
 		});
-
-	$rootScope.$on('$stateChangeStart', function(event, to, toParams, from, fromParams) {
-        console.log("hello");
-    });
+	})
 }]);
 
 app.config([
