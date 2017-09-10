16e0b1f6efb439ca818445792cd2b66764f809de
Kevin Yang <yangkevi@usc.edu>
2017-09-10 00:16:23 -0700

    add icon and some styling

--- chi/public/app/core/nav-bar/nav-bar.component.js
+++ chi/public/app/core/nav-bar/nav-bar.component.js
@@ -1,4 +1,12 @@
+let navBarController = [function() {
+  let ctrl = this;
+}]
+
 angular.module('app')
 .component('navBar', {
-  templateUrl: '/app/core/nav-bar/nav-bar.template.html'
+  templateUrl: '/app/core/nav-bar/nav-bar.template.html',
+  controller: navBarController,
+  bindings: {
+    hideSearchField: '<?'
+  }
 });
