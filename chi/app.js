ce6b44d034c83b3a774842c7f9f2223850643401
Kevin Yang <yangkevi@usc.edu>
2017-10-12 18:54:55 -0700

    add favicon

--- chi/app.js
+++ chi/app.js
@@ -10,13 +10,13 @@ var users = require('./routes/users');
 var api = require('./routes/api');
 
 var app = express();
+app.use(favicon(
+  path.join(__dirname, 'public', 'images', 'icons', 'favicon.ico')));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
